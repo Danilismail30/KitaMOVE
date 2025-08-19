7c897e0119e4b93d2a886b0e1cdef3e41ffeff92
Danilismail30 <echoslamproduction@gmail.com>
2025-08-19 18:39:50 +0800

    Kitamove Capstone Edited V1.1.2
    Debug route maps and proxy server

---
+++ kitamove-capstone/start-kitamove.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Starting KitaMOVE Application with API proxy...
+
+start cmd /k "cd /d %~dp0 && npm run dev"
+start cmd /k "cd /d %~dp0 && node malaysia-proxy.js"
+
+echo Both servers started successfully!
+echo React app: http://localhost:5174
+echo API proxy: http://localhost:5000
